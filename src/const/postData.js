13d1cc99d40129c4cdb8c8284a8ad6cabeb4ccbc
catt-wuyang <wwwxuer@126.com>
2022-08-03 13:42:49 +0800

    feat: update docs

--- src/const/postData.js
+++ src/const/postData.js
@@ -83,7 +83,7 @@ export const classify = [
         title: "生活如此冷漠，你却让我乐观的活着",
         description:
           "我想说看完长达 4 个小时的电影后，我整个人心里很闷，但最终却趋于平静和释然。身边的人包括我每天都会有各种各样的糟心烦人的事情要去面对解决，因为我们所要面对的不像电影里主人公们所要面对的事那么极端，所以我们至今还没有念头想去满洲里看那头一直坐在那的大象。但看完电影后，我倒是想去看看。",
-        image: "https://i.postimg.cc/kXRmQgGW/p2018263.webp",
+        image: "https://ibb.co/5RtRbBf",
         date: "Oct 16, 2018",
         pageid: "20181016",
       },
